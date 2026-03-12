4f5bb4d7bec0dadb6a7f48730cf3ff9b110e432c
Aulas Informáticas Universidad El Bosque <aulainformatica@unbosque.edu.co>
2026-03-11 19:55:54 -0500

    Proyecto React Native

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,47 @@
-/**
- * @format
- */
 
+import { StatusBar,  useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+import { CountScreen } from './Components';
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import { colors } from './themes';
+import { appStyles } from './App.style';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
-  });
-});
+
+
+type AppProps = {
+  atributo: String;
+}
+let count: number = 0;
+
+
+const handleOnPress = (value: number) => {
+  console.log("Press Button")
+  count += value;
+  console.log(count)
+}
+
+
+export class App extends React.Component <AppProps> {
+  render(){
+
+    const label = "hello world"
+    const name = "hello world x2"
+
+    console.log("App Funcionando")
+   return (
+
+          <View style={appStyles.safeArea}>
+            <view style={appStyles.container}>
+            <CountScreen label={label} OnPress={()=>handleOnPress(1)}></CountScreen>
+            <CountScreen label={name} OnPress={()=>handleOnPress(2)}></CountScreen>
+            </view>
+          </View>
+
+     
+      );
+  }
+}
+
+export default App;
