d83a52ee9745102b30b8e5c3ac37e895c761408d
Usuario <jdbuitragor@unbosque.edu.co>
2026-04-29 19:58:21 -0500

    avance login

---
+++ navigation/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native-gesture-handler";
+
+const App = () => {
+  return (
+    <Text>APP</Text>
+  )
+}
+
+export default App;
---
+++ navigation/src/components/templates/AuthTemplate/AuthTemplate.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native"
+import { Text } from "react-native-gesture-handler";
+
+interface AuthTemplateProps {
+    title: String;
+    subtitle: String;
+    children: React.ReactNode;
+}
+
+const AuthTemplate = ({
+    title, subtitle, children}: AuthTemplateProps) => {
+        return (
+            <View>
+                <Text>{title}</Text>
+                { subtitle && <Text>{subtitle}</Text>}
+                <View>
+                    {children}
+                </View>
+            </View>
+        );
+}
+
+export default AuthTemplate;
